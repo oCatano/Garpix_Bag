108ad1a7c9198881f20dc8f8c0da896ca5f07a26
vasilisy <m2009903@edu.misis.ru>
2022-07-12 18:09:05 +0300

    output file

--- Parser_and_other_functions.py
+++ Parser_and_other_functions.py
@@ -19,3 +19,26 @@ def count_bags(path: str):
     for i in groups:
         count += i['count']
     return count
+
+
+
+def get_cargo_space(path: str):
+    output_data = {}
+    space, groups = parser(path)
+    a = []
+    a = space['size']
+    cargoSpace = {'loading_size': {"height": 0, "length": 0, "width": 0}, 'position': [0,0,0], 'type': "pallet"}
+    cargoSpace['loading_size']["height"] = a[0] / 1000
+    cargoSpace['loading_size']['length'] = a[1] / 1000
+    cargoSpace['loading_size']['width'] = a[2] / 1000
+    cargoSpace['position'][0] = a[1] / 2000
+    cargoSpace['position'][1] = a[0] / 2000
+    cargoSpace['position'][2] = a[2] / 2000
+
+    output_data['cargoSpace'] = cargoSpace
+
+    with open('output_data_test.json', 'w') as f:
+        json.dump(output_data, f)
+
+    return output_data
+
