924a00711c82c943c066c42f63f3a4f0fb4a1da6
Starman-in-the-sky <ms.practice@yandex.ru>
2022-07-14 15:52:31 +0300

    Greedy algorithm alpha

--- Greedy algorithm 2D.py
+++ Greedy algorithm 2D.py
@@ -2,11 +2,13 @@ import json
 from BagClass import Bag
 from Parser_and_other_functions import parser
 from Parser_and_other_functions import count_bags
-'''class Bag:
+class Bags:
     def __size_space__(self):
         with open(path, "r") as json_file:
             data_list = json.load(json_file)
         c_space = dict(data_list['cargo_space']['size'])
+        c_space_edvard = (data_list['cargo_space']['id'])
+        c_space['id'] = c_space_edvard
         return c_space
     def __size_grops__(self):
         with open(path, "r") as json_file:
@@ -14,17 +16,22 @@ from Parser_and_other_functions import count_bags
         c_grops = data_list['cargo_groups']
         len_c_groups = len(c_grops)
         time_edvard = []
+        '''c_grops_edvard = (data_list['mass'])
+        c_grops['id'] = c_grops_edvard'''
         while len_c_groups != 0:
             len_c_groups = len_c_groups - 1
             data_size = dict(c_grops[len_c_groups])
             data_size_abc = (data_size["size"])
+            c_groups_edvard = (data_size['mass'])
+            data_size_abc['id'] = c_groups_edvard
             time_edvard.append(data_size_abc)
         c_grops = time_edvard
-        return c_grops'''
+
+        return c_grops
 
 
 path='_vg_85_bgg5jsons/125000/125018_cl.json'
-endi = Bag()
+endi = Bags()
 c_groups = endi.__size_grops__()
 c_cargo_space = endi.__size_space__()
 print(endi.__size_grops__())
@@ -58,6 +65,7 @@ def list_print(d_list):
                 print(k)
 
 
+'''Добавить Individ вместо d_list, d_list: Individ;   Также подаётся Cargo_space'''
 def fill_cargo(d_list, cargo_list):
     fullness_list = []
     array_of_base = [[0] * cargo_list['width'] for i in range(cargo_list['length'])]
@@ -72,13 +80,15 @@ def fill_row(d_list, cargo_list, f_list, arr_b):
 
 
 def fill_tower(d_list, cargo_list, arr_b, f_list, x_cor, y_cor):
+    last_w = cargo_list['width']
+    last_l = cargo_list['length']
     sum_of_high = 0
     num_of_boxes_in_the_tower = 0
     while(sum_of_high < cargo_list['high'] - find_the_smallest_high(d_list)):
-        sum_of_high += put_block(d_list, cargo_list, f_list, arr_b, x_cor, y_cor, sum_of_high, num_of_boxes_in_the_tower)
+        sum_of_high += put_block(d_list, cargo_list, f_list, arr_b, x_cor, y_cor, sum_of_high, num_of_boxes_in_the_tower, last_w, last_l)
 
 
-def put_block(d_list, cargo_list, f_list, arr_b, x_cor, y_cor, z_cor, num_b):
+def put_block(d_list, cargo_list, f_list, arr_b, x_cor, y_cor, z_cor, num_b, last_w, last_l):
     '''max_square = 0 в последнем листе'''
     high = 0
     for i in cargo_list(len(d_list), 0, -1):
@@ -93,19 +103,29 @@ def put_block(d_list, cargo_list, f_list, arr_b, x_cor, y_cor, z_cor, num_b):
                     for j in range(x_cor + i['length'], x_cor, -1):
                         for k in range(y_cor, y_cor + i['width'], 1):
                             arr_b[j][k] = 1
+                    break
                 else:
                     if(i['length'] < count_length(arr_b, y_cor) - x_cor):
-                        a = [[x_cor, y_cor, z_cor], [x_cor + i['length'], y_cor + i['width'], z_cor + i['high']], [i['length'], i['width'], i['high'], 0]]
+                        a = [[cargo_list['length'] - x_cor, y_cor, z_cor], [cargo_list['length'] - x_cor + i['length'], y_cor + i['width'], z_cor + i['high']], [i['length'], i['width'], i['high'], 0]]
                         f_list.append(a)
                         d_list[i].pop()
                         high = i['high']
                         for j in range(x_cor + i['length'], x_cor, -1):
                             for k in range(y_cor, y_cor + i['width'], 1):
                                 arr_b[j][k] = 1
+                        break
 
             else:
                 num_b+=1
+                if((i['length'] <= last_l) and (i['width'] <= last_w)):
+                    last_l = i['length']
+                    last_w = i['width']
+                    a = [[cargo_list['length'] - x_cor, y_cor, z_cor], [cargo_list['length'] - x_cor + i['length'], y_cor + i['width'], z_cor + i['high']], [i['length'], i['width'], i['high'], 0]]
+                    f_list.append(a)
+                    d_list[i].pop()
+                    high = i['high']
 
+                    
             break
     return high
 
