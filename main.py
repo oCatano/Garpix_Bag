4631330cfa7a5577bdff9969a2cd42cb71166f67
vasilisy <m2009903@edu.misis.ru>
2022-07-15 14:22:53 +0300

    output file (ura !!! pobeda!!!)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from Parser_and_other_functions import count_bags
-from Parser_and_other_functions import get_cargo_space
-#from Parser_and_other_functions import get_cargos
+from Parser_and_other_functions import ger_results
 
 from platform import python_version
 # _vg_85_bgg5jsons/104000/104004_cl.json
@@ -8,5 +7,4 @@ from platform import python_version
 # _vg_85_bgg5jsons/13000/13169_cl.json
 
 print(count_bags('_vg_85_bgg5jsons/0/100_cl.json'))
-print(get_cargo_space('_vg_85_bgg5jsons/0/100_cl.json'))
-#print(get_cargos('_vg_85_bgg5jsons/0/100_cl.json'))
+print(ger_results())
